2849249cb8f17d05e79b5f7d10290fa85e3774ec
Ahtisham-Shakir <ahtishamshakir777@gamil.com>
2022-08-31 14:16:53 +0500

    Worked on Header Component UI

--- slack-clone/src/components/header/Header.js
+++ slack-clone/src/components/header/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Header.css";
+import { Avatar } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="header__left"></div>
-      <div className="header__search"></div>
-      <div className="header__right"></div>
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="Ahtisham shakir" />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search here..." />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
     </div>
   );
 }
