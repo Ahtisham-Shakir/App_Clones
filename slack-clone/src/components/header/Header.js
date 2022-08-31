3d1a1255744288cd62770c2c3b0c1e2f0ad91587
Ahtisham-Shakir <ahtishamshakir777@gamil.com>
2022-08-31 12:51:20 +0500

    Initial setup

---
+++ slack-clone/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <div className="header__search"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
