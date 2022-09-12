98c187db1cb069adfec83e0b2cbc9370129640cc
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-09-12 12:30:33 +0500

    completed widgets component

--- twitter-clone/src/components/widgets/Widgets.js
+++ twitter-clone/src/components/widgets/Widgets.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "./Widgets.css";
 
+// react-twitter-embed
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function Widgets() {
-  return <div className="widgets">Widget</div>;
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId="1569200878069944323" />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton />
+      </div>
+    </div>
+  );
 }
