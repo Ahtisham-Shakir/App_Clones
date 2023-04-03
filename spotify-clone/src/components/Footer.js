75d3266ea57e7e1b6a77c1f6d9935ade37d3ec76
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-04 02:30:28 +0500

    created footer

--- spotify-clone/src/components/Footer.js
+++ spotify-clone/src/components/Footer.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./footer.css";
+import PlayArrowOutlinedIcon from "@mui/icons-material/PlayArrowOutlined";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import { Grid, Slider } from "@mui/material";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img className="footer__albumLogo" src="" alt="" />
+        <div className="footer__songInfo">
+          <h4>Title</h4>
+          <p>Song Playing</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayArrowOutlinedIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
